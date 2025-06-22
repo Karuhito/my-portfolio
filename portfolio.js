d8c4fa4a2a0ec0776f141cf90fc8da7731429784
Karuhito <179291384+Karuhito@users.noreply.github.com>
2025-06-22 22:08:49 +0900

    worksの内容を更新

--- portfolio.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener{'DOMContentLoaded', () => {
-    const elements = document.querySelectorAll('.fade-in');
-
-    const observer = new IntersectionObsever ((entries => {
-        entries.forEach( => {
-            
-        });
-    }))
-}}
--- script.js
+++ script.js
@@ -20,3 +20,14 @@ function showElementAnimation() {
 }
 showElementAnimation();
 window.addEventListener('scroll', showElementAnimation);
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	  background(255);
+	}
+	
+	function draw() {
+		fill(random(256), random(256), random(256), random(50, 201));
+		noStroke();
+		circle(random(mouseX-80, mouseX-81), random(mouseY-80, mouseY+81),random(10, 41));
+	}
